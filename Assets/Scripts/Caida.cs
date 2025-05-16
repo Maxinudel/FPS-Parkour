a1c0f19cd9426a9b87d958aa74b1ed3a4fd77d61
Maxinudel <166538999+Maxinudel@users.noreply.github.com>
2025-05-16 09:03:53 -0300

    Listo

---
+++ Assets/Scripts/Caida.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Caida : MonoBehaviour
+{
+    public float restart = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < restart)
+        {
+
+            Debug.Log("Respawm");
+            SceneManager.LoadScene("FPS Parkour");
+        }
+    }
+}
